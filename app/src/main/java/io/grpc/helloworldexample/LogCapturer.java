6cfb46a9302be6688fb18ea72d8b28c84279349b
stdStudent <78802930+stdStudent@users.noreply.github.com>
2023-08-02 14:38:15 +0500

    Added buffering.

--- app/src/main/java/io/grpc/helloworldexample/LogCapturer.java
+++ app/src/main/java/io/grpc/helloworldexample/LogCapturer.java
@@ -6,12 +6,24 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LogCapturer {
     private Thread captureThread;
     private OutputStream logcatOutput;
+    private Boolean saveBuffered = true;
 
+    private List<String> buffer;
+
+    public void startCapture(Boolean saveToBuf) {
+        this.saveBuffered = saveToBuf;
+        startCapture();
+    }
     public void startCapture() {
+        if (saveBuffered)
+            buffer = new ArrayList<String>();
+
         captureThread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -25,6 +37,8 @@ public class LogCapturer {
                     while ((line = bufferedReader.readLine()) != null) {
                         logcatOutput.write(line.getBytes());
                         logcatOutput.write(System.lineSeparator().getBytes());
+
+                        if (saveBuffered) buffer.add(line);
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -38,7 +52,19 @@ public class LogCapturer {
         return logcatOutput;
     }
 
+    public List<String> getLogcatOutputBuffered() {
+        return buffer;
+    }
+
     public String[] getCapturedLogs() {
+        if (logcatOutput == null) {
+            return new String[0];
+        }
+
+        if (saveBuffered) {
+            return buffer.toArray(new String[buffer.size()]);
+        }
+
         String buffer = logcatOutput.toString();
         return buffer.split("\n");
     }
